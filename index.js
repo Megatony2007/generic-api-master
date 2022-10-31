107feb9adb0eeabffba67f36c950a841e75ec5d6
Megatony <antoniojsmoraes@outlook.com>
2022-10-31 19:59:32 -0400

    teste

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+)
+
+app.use(express.json());
+
+const clienteRoutes = require('./routes/clienteRoutes');
+
+app.use('/clientes', clienteRoutes);
+
+app.get('/', (req, res) => {
+    res.json("Generic API!!")
+    return
+});
+
+mongoose.connect('mongodb+srv://megaatony:foimal2022@cluster.ixk9ogi.mongodb.net/?retryWrites=true&w=majority')
+    .then(() => {
+        console.log('Chegamos ao DB!')
+        app.listen(3008)
+    })
+    .catch((err) => console.log(err))
+
+
+
+app.listen(3000)
